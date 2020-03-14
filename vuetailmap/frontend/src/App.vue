3cd518ea9ede7f3c1b61878fbd986f43727b8771
richardmiddleton <richardmiddleton@numiko.com>
2020-03-14 13:29:41 +0000

    setup and tailwind installed

--- vuetailmap/frontend/src/App.vue
+++ vuetailmap/frontend/src/App.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="container bg-red-200" id="app">
-        <!-- <h2 class="text-3xl">{{ home[0].title }}</h2>
-        <img v-if="home[0].image" :src="'http://localhost:8000' + home[0].image.url" alt=""> -->
+        <h2 class="text-3xl">{{ home[0].title }}</h2>
+        <img v-if="home[0].image" :src="'http://localhost:8000' + home[0].image.url" alt="">
   </div>
 </template>
 
 <script>
 
-// import './assets/styles/index.css';
 
 export default {
   name: 'App',
